b1e13070ed5c81fd2825a88b372bc58cb3483693
dhens <dawsonhensel@gmail.com>
2020-07-18 11:09:07 -0700

    this current commit will be used for another repo of a mern boikerplate, ignore the package.json name and description values. added mongo connection to server.js and routes file

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.send('/ route hit!');
+})
+
+router.post('/post', (req, res) => {
+    res.send(`body received: ${JSON.stringify(req.body)}`);
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const express = require("express");
-const path = require("path");
+require('dotenv').config()  // Initialize our protected environment variables
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose')
+const routes = require('./routes');
+
+app.use(express.urlencoded({ extended: true })); // Parses incoming requests with urlencoded payloads 
+app.use(express.json()); // Parses incoming requests with JSON payloads 
+app.use(routes); // Backend routes will come from our routes file
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to our mongo database server
+try {
+  mongoose.connect(process.env.MONGODB_URI, {
+    // use mongodb v4 connection settings
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });  
+} catch (err) {
+  `Connection to mongo server failed: ${err}`;
+}
 
+// Start server
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
