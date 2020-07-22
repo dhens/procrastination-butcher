0379aa0d7c91110b24de20566e61ee4f882bfde8
dhens <dawsonhensel@gmail.com>
2020-07-22 09:57:52 -0700

    added env example file

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.send('/ route hit!');
-})
+});
 
 router.post('/post', (req, res) => {
     res.send(`body received: ${JSON.stringify(req.body)}`);
-})
+});
+
 module.exports = router;
